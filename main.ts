97033599e3b3118313ef87da68866e60d1fa62ed
asmajawaid <arshadasma45678@gmail.com>
2024-04-18 21:32:22 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,48 @@
+#! usr/bin/env node 
+
+import inquirer from "inquirer";
+
+console.log("\n Welcome to currency cunverter \n")
+
+const currency:any = {
+  USD: 1, //Base currency
+  EUR: 0.91,
+  GBP: 0.76,
+  INR: 74.57,
+  PKR: 280,
+};
+
+let user_answer = await inquirer.prompt([
+  {
+    name: "from",
+    message: "Enter from currency",
+    type: "list",
+    choices: ["USD", "EUR", "GBP", "INR", "PKR"],
+  },
+  {
+    name: "To",
+    message: "Enter from currency",
+    type: "list",
+    choices: ["USD", "EUR", "GBP", "INR", "PKR"],
+  },
+  {
+    name: "Amount",
+    message: "Enter your amount",
+    type: "number"
+  }
+]);
+
+
+//It is dynamic Approch
+let fromAmount = currency[user_answer.from] //exchange rate
+
+let toAmount =currency[user_answer.To] //exchange rate
+
+let amount =user_answer.Amount
+
+let baseAmount = amount/fromAmount //USD base currency
+//               15000/280         // formula
+
+let convertedAmount = baseAmount * toAmount
+let total = convertedAmount.toFixed(2);
+console.log(total);
